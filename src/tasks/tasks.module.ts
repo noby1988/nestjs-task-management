8e9b01b4080c52235a9417ce9d2704d715619d0d
Noble Babu Mathew <nobyyy@gmail.com>
2025-05-19 19:01:21 +0200

    Finished Authentication

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -5,8 +5,9 @@ import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { TasksDataService } from './tasks-data.service';
+import { AuthModule } from 'src/auth/auth.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService, TasksDataService],
 })
